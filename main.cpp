f647a422db4d2dd7fb09974fb1fc64ac3c285c62
Nikita Vadimov <pipyaka3@gmail.com>
2017-04-11 23:48:08 +0300

    Project started

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by vaace on 4/11/17.
+//
+
+#include "main.h"
+
+#include <iostream>
+#include <math.h>
+
+
+int main() {
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by vaace on 4/11/17.
+//
+
+#ifndef FINAL_PROJECT_MAIN_H
+#define FINAL_PROJECT_MAIN_H
+
+#endif //FINAL_PROJECT_MAIN_H
